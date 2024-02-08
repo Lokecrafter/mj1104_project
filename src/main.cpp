1e180ecc1ca2f14085c40e5b250cfd2e47c609b2
Lokecrafter <66024236+Lokecrafter@users.noreply.github.com>
2024-02-08 11:51:24 +0100

    Initial tests of solar car

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,48 @@
 #include <Motor.h>
 
 
-Motor motorA(5,6);
-Motor motorB(10,11);
+Motor motorRight(6,5);
+Motor motorLeft(10,11);
 
 void setup() {
 	Serial.begin(9600);
-	motorA.driveVelocity(-1);
+	motorLeft.driveVelocity(-1);
 
 	pinMode(A0, INPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-	Serial.println(digitalRead(A0));
-	/*
-	motorA.driveVelocity(1);
-	//motorB.driveVelocity(-0.75);
+	motorLeft.driveVelocity(1);
+	//motorRight.driveVelocity(1);
 	Serial.println("Full speed");
+
+	digitalWrite(LED_BUILTIN, HIGH);
 	delay(1000);
-	motorA.stop();
-	motorB.stop();
+	motorLeft.stop();
+	motorRight.stop();
+
+	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
-	motorA.driveVelocity(-0.75);
-	//motorB.driveVelocity(0.75);
+	//motorLeft.driveVelocity(-0.75);
+	motorRight.driveVelocity(1);
+	
+	digitalWrite(LED_BUILTIN, HIGH);
 	delay(1000);
-	motorA.stop();
-	motorB.stop();
+	motorLeft.stop();
+	motorRight.stop();
+	
+	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
-	*/
 }
+
+
+
+
+/*
+Teorier
+
+Kanske kan funka att köra långsamt men håller i ungefär 3-5 sekunder efter datorn har laddat upp kondensatorerna.
+
+
+*/
