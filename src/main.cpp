8062ca8d6813f6c17dcf4c52b4d91621646bae51
Lokecrafter <66024236+Lokecrafter@users.noreply.github.com>
2024-02-29 10:26:00 +0100

    tried constants

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,17 @@ int sensorLeft = 3;
 int sensorRight = 4;
 
 //Robot variables
-const float velocityForward = 1;
-const float maxXDrive = 1;
+const float velocityForward = 0.7;
+const float maxXDrive = 0.7;
 
 //PID constants
 const float kp = 1;
-const float ki = 1;
+const float ki = 0.5;
 const float kd = 0;
 
 //Integration constants
-const float errorSumMultiplier = 1;
-const float errorDecayConstant = 0.5;
+const float errorSumMultiplier = 50;
+const float errorDecayConstant = 6;
 const float maxErrorSum = 1;
 
 //Global variables for time, integral and derivative
@@ -44,11 +44,17 @@ void driveRobot(float x, float y){
 	float leftDrive = y + x;
 	float rightDrive = y - x;
 
+	Serial.print("   left: ");
+	Serial.print(leftDrive);
+	Serial.print("   right: ");
+	Serial.println(rightDrive);
+
 	motorLeft.driveVelocity(leftDrive);
 	motorRight.driveVelocity(rightDrive);
 }
 
 void loop() {
+
 	float deltaTime = (micros() - lastMicros) / (float)1000000;
 	lastMicros = micros();
 
@@ -58,15 +64,22 @@ void loop() {
 	//Calculate "Turn factor" with PID controller
 	float deltaError = (error - lastError) / deltaTime;
 	float xDrive = kp * error + ki * sumError + kd * deltaError;	//PID controller
+	Serial.print("sumError: ");
+	Serial.print(sumError);
+	Serial.print("   xDrive: ");
+	Serial.print(xDrive);
 
 	driveRobot(xDrive, velocityForward);
 
 	//Integrate error and add an errordecay by numeric differential equation
+	float firstSumErrorDerivative = sumError * errorDecayConstant; 
+	sumError -= firstSumErrorDerivative * deltaTime;
+
+
 	sumError += error * errorSumMultiplier * deltaTime;
 	sumError = min(max(sumError, -maxErrorSum), maxErrorSum); //Clamp error sum
 
-	float firstSumErrorDerivative = sumError * errorDecayConstant; 
-	sumError -= firstSumErrorDerivative * deltaTime;
+	
 
 	lastError = error;
 }
