43e4780a1b102c262ed2337ad4b7f3eea0f55c4e
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2024-05-20 00:00:26 +0200

    Added comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,41 @@
 
 Motor motorRight(6,5);
 Motor motorLeft(10,11);
-
-bool isDrivingRight = false;
-bool driveWithMemory = false;
 int sensorLeft = 3;
 int sensorRight = 4;
 
-float speed = 1;
-
 int chargeTimeSeconds = 20;
 
-void setup() {
-	Serial.begin(9600);
+bool isDrivingRight = false;
+bool driveWithMemory = false;
+float speed = 1;
 
+void setup() {
 	pinMode(sensorLeft, INPUT);
 	pinMode(sensorRight, INPUT);
-
 
 	delay(chargeTimeSeconds * 1000);
 }
 
 void loop() {
-	if(!digitalRead(sensorLeft)){
+	if(!digitalRead(sensorLeft)){ //Turn left
 		isDrivingRight = false;
 		driveWithMemory = false;
 		motorRight.driveVelocity(speed);
 		motorLeft.driveVelocity(-speed/3);
 	}
-	else if(!digitalRead(sensorRight)){
+	else if(!digitalRead(sensorRight)){ //Turn right
 		isDrivingRight = true;
 		driveWithMemory = false;
 		motorLeft.driveVelocity(speed);
 		motorRight.driveVelocity(-speed/3);
 	}
-	else{
+	else{ //Continue turning based on memory
 		driveWithMemory = true;
 	}
 
+	//Continue turning based on memory
 	if(driveWithMemory){
-
 		if(isDrivingRight){
 			motorLeft.driveVelocity(speed);
 			motorRight.stop();
@@ -52,23 +48,4 @@ void loop() {
 			motorLeft.stop();
 		}
 	}
-
-
-	Serial.print("Sensor Left: ");
-	Serial.print(digitalRead(sensorLeft));
-	Serial.print("   Sensor right: ");
-	Serial.print(digitalRead(sensorRight));
-	Serial.print("   Driving right: ");
-	Serial.println(isDrivingRight);
 }
-
-
-
-
-/*
-Teorier
-
-Kanske kan funka att köra långsamt men håller i ungefär 3-5 sekunder efter datorn har laddat upp kondensatorerna.
-
-
-*/
